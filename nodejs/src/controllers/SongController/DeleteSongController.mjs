2349d245ab0a7ff518507b7249cc1ccc14ed893d
magnojunior07 <magnojunior319@gmail.com>
2023-03-06 22:12:11 -0400

    fix delete song bugs

--- nodejs/src/controllers/SongController/DeleteSongController.mjs
+++ nodejs/src/controllers/SongController/DeleteSongController.mjs
@@ -2,9 +2,10 @@ import DeleteSongServices from "../../services/DeleteSongService.mjs";
 
 export default class DeleteSongController{
     
-    async handleDeleteSong(res, req ){
+    async handleDeleteSong(req, res){
         const song = req.body;
-
+        song.id = req.params.id;
+        
         try {
             const deleteSong = await new DeleteSongServices().deleteSong(song)
             res.json(deleteSong);
--- nodejs/src/services/DeleteSongService.mjs
+++ nodejs/src/services/DeleteSongService.mjs
@@ -1,17 +1,18 @@
 import prisma from "../utils/prisma.mjs";
 
 export default class DeleteSongServices{
-        async deleteSong(res, req, song){
+        async deleteSong(song){
             try {
-                const deleteSong = await prisma.charlie_brown_jr_songs.delete({
+                const deletedSong = await prisma.charlie_brown_jr_songs.delete({
                     where: {
                         id: Number(song.id)
                     }
-                })
+                });
 
-                return res.json(`Song ${deleteSong} it is deleted successfully`)
+                return deletedSong
+                
         }catch(err) {
-            return res.json(err)
+            return err
 
         }
     }
